c47aba9713370803d891ee5a92733a465a961afb
SlavikChyzh <110846906+SlavikChyzh@users.noreply.github.com>
2023-02-11 13:29:03 +0200

    done
    done

--- src/markUp.js
+++ src/markUp.js
@@ -11,17 +11,16 @@ function createMarkUp(arr, gallery) {
       downloads,
     } = el;
     acc += `
-      <div class="gallery-item" id="${id}">
-        <a class="gallery__link" href="${largeImageURL}">
+        <a class=" gallery-item" href="${largeImageURL}">
           <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-        </a>
         <div class="info">
           <p class="info-item"><b>Likes</b>${likes}</p>
           <p class="info-item"><b>Views</b>${views}</p>
           <p class="info-item"><b>Comments</b>${comments}</p>
           <p class="info-item"><b>Downloads</b>${downloads}</p>
         </div>
-      </div>
+        </a>
+        
     `;
 
     return acc;
